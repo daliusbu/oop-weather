a299e5c9a9632c44c16ae387bce6bb208b3c095f
daliusbu@gmail.com <daliusbu@gmail.com>
2019-05-01 14:54:16 +0300

    Google Api works, JSON - works only with Today

---
+++ src/Weather/Model/JsonWeather.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dalius
+ * Date: 19.5.1
+ * Time: 12.54
+ */
+
+namespace Weather\Model;
+
+
+class JsonWeather
+{
+
+}
---
+++ src/Weather/Model/NullJasonWeather.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dalius
+ * Date: 19.5.1
+ * Time: 14.47
+ */
+
+namespace Weather\Model;
+
+
+class NullJasonWeather
+{
+
+}
